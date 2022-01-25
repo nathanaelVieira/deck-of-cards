6bd92b505c170aa95c233c16c98852919f8c75de
nathanael <vieira_nathanael@hotmail.com>
2022-01-24 23:15:59 -0300

    Sms deck of the cards

---
+++ src/entities/Card.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Card {
+
+	private final String face;
+	private final String suit;
+
+	public Card(String face, String suit) {
+		this.face = face;
+		this.suit = suit;
+	}
+
+	@Override
+	public String toString() {
+		return face + " of" + suit;
+	}
+
+}
